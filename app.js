1f2e7367c07a8b97e78c39ab3403e7c47e5e5aa1
Renan Serrão Nogueira <renannogueira@getwayautomacao.com.br>
2017-07-24 10:11:03 -0300

    ae

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
 'use strict';
 
-const app = require('express')();
-const server = null;
-
-module.exports = app;
-
-const io = require('socket.io').listen(server);
-var clients = {};
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
     res.status(200).send('O servidor está inicializado com sucesso! Porta ' + app.get('port'));
 });
 
-io.on("connection", function(client) {
-    client.on("join", function(name) {
-        clients[client.id] = name;
-        clients[client.color] = getRandomColor();
-        console.log("Joined: " + name + " Color: " + clients[client.color]);
-        client.emit("login", clients[client.color]);
-        client.emit("update", "Você entrou no bate-papo.", clients[client.color]);
-        client.broadcast.emit("update", name + " entrou no bate-papo.", clients[client.color])
+if (module === require.main) {
+    const server = app.listen(process.env.PORT || 8081, () => {
+        const port = server.address().port;
+        console.log(`App listening on port ${port}`);
     });
 
-    client.on("send", function(msg) {
-        console.log("Message: " + msg + " Color: " + clients[client.color]);
-        client.broadcast.emit("chat", clients[client.id], msg, clients[client.color]);
-    });
+    const io = require('socket.io').listen(server);
+    var clients = {};
 
-    client.on("disconnect", function() {
-        console.log("Disconnect");
-        io.emit("update", clients[client.id] + " saiu do bate-papo.", clients[client.color]);
-        delete clients[client.id];
+
+    io.on("connection", function(client) {
+        client.on("join", function(name) {
+            clients[client.id] = name;
+            clients[client.color] = getRandomColor();
+            console.log("Joined: " + name + " Color: " + clients[client.color]);
+            client.emit("login", clients[client.color]);
+            client.emit("update", "Você entrou no bate-papo.", clients[client.color]);
+            client.broadcast.emit("update", name + " entrou no bate-papo.", clients[client.color])
+        });
+
+        client.on("send", function(msg) {
+            console.log("Message: " + msg + " Color: " + clients[client.color]);
+            client.broadcast.emit("chat", clients[client.id], msg, clients[client.color]);
+        });
+
+        client.on("disconnect", function() {
+            console.log("Disconnect");
+            io.emit("update", clients[client.id] + " saiu do bate-papo.", clients[client.color]);
+            delete clients[client.id];
+        });
     });
-});
+}
+
+module.exports = app;
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -41,11 +49,4 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-}
-
-if (module === require.main) {
-    server = app.listen(process.env.PORT || 8081, () => {
-        const port = server.address().port;
-        console.log(`App listening on port ${port}`);
-    });
 }
